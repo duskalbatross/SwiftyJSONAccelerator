1a73c9b15b98b7a3d011da206d5121efc9a7c458
李颖晖 <yinghui.li@xixicloud.com>
2017-09-10 17:10:48 +0800

    生成的SwiftyJSON类全部实现SwiftyJSONable协议；值为null的字段生成属性，默认类型为String

--- Core/Library-Extensions/SwiftyJSONModelFile.swift
+++ Core/Library-Extensions/SwiftyJSONModelFile.swift
@@ -38,7 +38,7 @@ struct SwiftyJSONModelFile: ModelFile, DefaultModelFileComponent {
     }
 
     func baseElementName() -> String? {
-        return nil
+        return "SwiftyJSONable"
     }
 
     func mainBodyTemplateFileName() -> String {
@@ -83,7 +83,18 @@ struct SwiftyJSONModelFile: ModelFile, DefaultModelFileComponent {
             component.decoders.append(genDecoder(property.name, "Any", property.constantName, true))
             component.encoders.append(genEncoder(property.name, "Any", property.constantName))
         case .nullType:
-            // Currently we do not deal with null values.
+            // null类型必须处理，虽然值为null，也需要生成模型，处理过程和.valueType一样
+            // 唯一的问题是值是null的话，无法确定类型，默认是使用any?类型
+            component.stringConstants.append(genStringConstant(property.constantName, property.key))
+            component.initialisers.append(genInitializerForVariable(property.name, property.type, property.constantName))
+            
+            // null类型默认使用string，string比较通用，转换成其它类型也比较容易，空数组返回的是[]可以识别
+            component.declarations.append(genVariableDeclaration(property.name, "String", false))
+            
+            component.description.append(genDescriptionForPrimitive(property.name, property.type, property.constantName))
+            component.decoders.append(genDecoder(property.name, property.type, property.constantName, false))
+            component.encoders.append(genEncoder(property.name, property.type, property.constantName))
+
             break
         }
     }
